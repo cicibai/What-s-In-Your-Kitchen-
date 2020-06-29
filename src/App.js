0ac3a6464d38f7963991214e5bd74dd75ac8b0b1
Cici Bai <34460231+cicibai@users.noreply.github.com>
2020-06-28 20:33:44 -0700

    jo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const APP_ID = '9d6f1293';
+    const APP_KEY = 'dbd604ae3d8e49c1c78890586e39d73d';
+    const example_Req = 
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(()=> {
+        console.log("Effect has been run");
+    },[]);
+
+    return (
+        <div className="App">
+            <form className="search-form">
+                <input className="search-bar" type="text" />
+                <button className="search-button" type="submit">
+                  Search
+                </button>
+            </form>
+            <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+        </div>
+    );
+};
 
 export default App;
+
