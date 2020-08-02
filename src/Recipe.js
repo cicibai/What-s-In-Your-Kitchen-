57e21d534fe8720568ad8e233fee83ea3d8ac8a7
lindama <lindima200@gmail.com>
2020-08-01 20:35:11 -0600

    list now bolds searched ingredients

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,25 +4,30 @@ import './App.js';
 
 
 const Recipe = ({title,image, ingredients, url, history}) => {
+    var requiredIngredients = [];
     return(
         <div className={style.recipe}>
             <h1>{title}</h1>
-            <o1>
+            <ul>
                 {history.split(",").map(item => (
+                    (item === "" ? console.log("Empty string") : 
                     ingredients.map((ingredient) => (
-                        //USE FILTER!!!
-                         (ingredient.text.includes(item.toLowerCase()) ? <li><b>{ingredient.text}</b></li> : console.log("Doesn't match"))
-                    ))
-                   
-                ))}
-            </o1>
+                         (ingredient.text.includes(item.toLowerCase()) ? <li><b>{ingredient.text}</b></li> : checkArray(requiredIngredients, ingredient.text))
+                    )))))}
+                    {requiredIngredients.map(requiredIngredient => 
+                        <li>{requiredIngredient}</li>)}
+            </ul>
             <img src={image} alt=""/>
             <h4><a href={url}>Click for full recipe</a></h4>
 
         </div>
     )
+}
 
-
+function checkArray(ingreList, ing) {
+    if (!ingreList.includes(ing)) {
+        ingreList.push(ing)
+    }
 }
 
 export default Recipe;
